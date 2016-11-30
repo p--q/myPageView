6f20e7c1d64e0746d7f2136326825169b7d942d9
pq <pq.blogspot@gmail.com>
2016-11-30 22:00:11 +0900

    完成

--- public_html/mypageview.js
+++ public_html/mypageview.js
@@ -1,57 +1,55 @@
     var myPageView = myPageView || function(){
         pv = {
-            buttuns:{
-                setCookie: function(year){
-                    document.cookie="_ns=2;expires=Sun, 09 Aug " + year + " 11:53:58 GMT;domain=." + vars.domein + ";path=/;";
-                    Display();               
-                }
-            },
-            all: function(){
+            all: function(id){
+                var elem = document.getElementById(id);
+                var h3Node = createElem("h3");
                 if(!navigator.cookieEnabled){
-                  vars.h3Node.textContent = "cookieを有効にしてください。";
-                  vars.elem.appendChild(vars.h3Node);
+                  h3Node.textContent = "cookieを有効にしてください。";
+                  elem.appendChild(h3Node);
                   return;
                 }
-                vars.aNode.href = "#";
-                vars.divNode.style.color = "#cccccc";
-                Display(); 
+                var aNode = createElem("a");
+                aNode.href = "#";
+                var divNodeLink = createElem("div");
+                divNodeLink.appendChild(aNode);
+                var divNodeOffLink = createElem("div");
+                divNodeOffLink.style.color = "#cccccc";
+                Display([elem, h3Node, divNodeLink, divNodeOffLink]); 
             }
         };  // end of pv
         var vars ={
-            elem: document.getElementById("myPageView"),
-            h3Node: document.createElement('h3'),
             domein: location.hostname,
-            aNode: document.createElement("a"),
             on_msg: "自分のページビューを追跡に変更する。",
-            off_msg: "自分のページビューを追跡しないに変更する。",
-            divNode: document.createElement('div'),
-            childelems: []
-        };
-        
-        function trackOff(){
-            vars.h3Node.textContent = "この"+vars.domein+"はページビューを追跡していません。"; 
-            vars.aNode.onclick = function(){myPageView.buttuns.setCookie(2000);return false;};
-            vars.aNode.textContent = vars.on_msg;
-            vars.divNode.textContent = vars.off_msg;
-            vars.childelems = [vars.h3Node, vars.aNode, vars.divNode];           
+            off_msg: "自分のページビューを追跡しないに変更する。"
+        };     
+        function createElem(tag){
+            return document.createElement(tag); 
         }
-        function trackOn(){
-            vars.h3Node.textContent = "この"+vars.domein+"はページビューを追跡しています。"; 
-            vars.aNode.onclick = function(){myPageView.buttuns.setCookie(2030);return false;};
-            vars.aNode.textContent = vars.off_msg;
-            vars.divNode.textContent = vars.on_msg;
-            vars.childelems = [vars.h3Node, vars.divNode, vars.aNode];             
+        function trackOff(elems){
+            elems[1].textContent = "この"+vars.domein+"はページビューを追跡していません。"; 
+            elems[2].firstChild.onclick = function(){setCookie(2000, elems);return false;};
+            elems[2].firstChild.textContent = vars.on_msg;
+            elems[3].textContent = vars.off_msg;
+            return [].concat(elems);           
         }
-        function Display(){
-            if(document.cookie.match("_ns=2")){
-                trackOff();
-            } else {
-                trackOn();                
-            }
-            if (vars.childelems) {
-                vars.childelems.forEach(function(n){vars.elem.appendChild(n);});
+        function trackOn(elems){
+            elems[1].textContent = "この"+vars.domein+"はページビューを追跡しています。"; 
+            elems[2].firstChild.onclick = function(){setCookie(2030, elems);return false;};
+            elems[2].firstChild.textContent = vars.off_msg;
+            elems[3].textContent = vars.on_msg;
+            return [elems[0], elems[1], elems[3], elems[2]];              
+        }
+        function Display(elems){
+            e = (document.cookie.match("_ns=2"))?trackOff(elems):trackOn(elems);
+            if (e) {
+                var elem = e.shift();
+                e.forEach(function(n){elem.appendChild(n);});
             }                 
+        }
+        function setCookie(year, elems){
+            document.cookie="_ns=2;expires=Sun, 09 Aug " + year + " 11:53:58 GMT;domain=." + vars.domein + ";path=/;";
+            Display(elems);               
         }
         return pv;
     }();
-    myPageView.all();
+    myPageView.all("myPageView");
