0dc4a2e15d3e50a7ebc3c4741ed1e049210e5400
pq <pq.blogspot@gmail.com>
2016-11-30 22:33:45 +0900

    コメント挿入。

--- public_html/mypageview.js
+++ public_html/mypageview.js
@@ -1,55 +1,57 @@
+    // myPageViewモジュール
     var myPageView = myPageView || function(){
-        pv = {
-            all: function(id){
-                var elem = document.getElementById(id);
-                var h3Node = createElem("h3");
-                if(!navigator.cookieEnabled){
-                  h3Node.textContent = "cookieを有効にしてください。";
-                  elem.appendChild(h3Node);
-                  return;
+        var pv = { // グローバルスコープに出すオブジェクト。グローバルスコープから呼び出すときはmyPageViewになる。
+            all: function(id){ // ここから開始する。引数にページビュー設定に置換する要素のidを入れる。
+                var elem = document.getElementById(id);  // idの要素を取得。
+                var h3Node = createElem("h3");  // 現在の状態を表示するh3タグの要素を作成。
+                if(!navigator.cookieEnabled){  // cookieが有効でない場合。
+                  h3Node.textContent = "cookieを有効にしてください。";  // h3タグの要素のテキストに代入。
+                  elem.appendChild(h3Node);  // idの要素の子要素に追加して表示させる。
+                  return;  // スクリプト終わり。
                 }
-                var aNode = createElem("a");
-                aNode.href = "#";
-                var divNodeLink = createElem("div");
-                divNodeLink.appendChild(aNode);
-                var divNodeOffLink = createElem("div");
-                divNodeOffLink.style.color = "#cccccc";
-                Display([elem, h3Node, divNodeLink, divNodeOffLink]); 
+                h3Node.style.paddingBottom = "20px";  // h3タグの要素の下部に20px空ける。
+                var aNode = createElem("a");  // aタグの要素を作成。
+                aNode.href = "#";  // aタグの要素のhrefを設定。
+                var divNodeLink = createElem("div");  // aNodeの親要素となるdivタグの要素を作成。
+                divNodeLink.appendChild(aNode);  // aNodeをdivNodeLinkの要素の子要素にする。
+                var divNodeOffLink = createElem("div");  // aタグ要素ない方のdivタグの要素を作成。
+                divNodeOffLink.style.color = "#cccccc";  // divNodeOffLinkの色を設定。
+                Display([elem, h3Node, divNodeLink, divNodeOffLink]); // 子要素以外を配列で渡す。
             }
         };  // end of pv
-        var vars ={
-            domein: location.hostname,
-            on_msg: "自分のページビューを追跡に変更する。",
-            off_msg: "自分のページビューを追跡しないに変更する。"
+        var vars ={ // モジュール内の"グローバル"変数。
+            domein: location.hostname,  // ブログのドメイン。
+            on_msg: "自分のページビューを追跡に変更する。",  // 2行目に表示させるメッセージ。
+            off_msg: "自分のページビューを追跡しないに変更する。"  // 3行目に表示させるメッセージ。
         };     
-        function createElem(tag){
+        function createElem(tag){  // tagの要素を作成して返す。
             return document.createElement(tag); 
         }
-        function trackOff(elems){
+        function trackOff(elems){  // ページビューを追跡していないときの表示を作成。
             elems[1].textContent = "この"+vars.domein+"はページビューを追跡していません。"; 
-            elems[2].firstChild.onclick = function(){setCookie(2000, elems);return false;};
+            elems[2].firstChild.onclick = function(){setCookie(2000, elems);return false;};  // aタグのプロパティを設定。
             elems[2].firstChild.textContent = vars.on_msg;
-            elems[3].textContent = vars.off_msg;
-            return [].concat(elems);           
+            elems[3].textContent = vars.off_msg;  // aタグがない方のプロパティを設定。
+            return [].concat(elems);  // 配列は参照渡しなのでコピーを返す。          
         }
-        function trackOn(elems){
+        function trackOn(elems){  // ページビューを追跡しているときの表示を作成。
             elems[1].textContent = "この"+vars.domein+"はページビューを追跡しています。"; 
-            elems[2].firstChild.onclick = function(){setCookie(2030, elems);return false;};
+            elems[2].firstChild.onclick = function(){setCookie(2030, elems);return false;};  // aタグのプロパティを設定。
             elems[2].firstChild.textContent = vars.off_msg;
-            elems[3].textContent = vars.on_msg;
-            return [elems[0], elems[1], elems[3], elems[2]];              
+            elems[3].textContent = vars.on_msg;  // aタグがない方のプロパティを設定。
+            return [elems[0], elems[1], elems[3], elems[2]];  // aタグがある方の要素を最後に表示させるように配列の順を入れ替えて返す。    
         }
-        function Display(elems){
-            e = (document.cookie.match("_ns=2"))?trackOff(elems):trackOn(elems);
+        function Display(elems){  // 要素の配列を引き数にしてidの要素の子要素に追加して表示させる。
+            e = (document.cookie.match("_ns=2"))?trackOff(elems):trackOn(elems);  // _ns=2のcookieがあると追跡していない、ないと追跡している。
             if (e) {
-                var elem = e.shift();
-                e.forEach(function(n){elem.appendChild(n);});
+                var elem = e.shift();  // 要素の配列からidの要素を切り出す。
+                e.forEach(function(n){elem.appendChild(n);});  // idの要素以外の要素を順番にidの要素の子要素に追加して表示させる。
             }                 
         }
-        function setCookie(year, elems){
-            document.cookie="_ns=2;expires=Sun, 09 Aug " + year + " 11:53:58 GMT;domain=." + vars.domein + ";path=/;";
-            Display(elems);               
+        function setCookie(year, elems){  // aタグの要素がクリックされた時に起動する関数。
+            document.cookie="_ns=2;expires=Sun, 09 Aug " + year + " 11:53:58 GMT;domain=." + vars.domein + ";path=/;";  // cookieの有効期限を過去にするとcookieが消える。
+            Display(elems);  // 表示の更新。
         }
-        return pv;
+        return pv;  // グローバルスコープにpvを返す。
     }();
-    myPageView.all("myPageView");
+    myPageView.all("myPageView");  // これでmyPageViewモジュールを起動する。引き数はページビュー設定を表示させる要素のid。
