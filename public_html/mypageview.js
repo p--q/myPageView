237660c4e76384de857c8493a9b16fefed1ca4f1
pq <pq.blogspot@gmail.com>
2016-12-01 15:10:16 +0900

    句読点の文字化け修正。chTDL()のバグ修正。

--- public_html/mypageview.js
+++ public_html/mypageview.js
@@ -21,21 +21,21 @@
         };  // end of pv
         var vars ={ // モジュール内の"グローバル"変数。
             domein: location.hostname,  // ブログのドメイン。
-            on_msg: "自分のページビューを追跡に変更する。",  // 2行目に表示させるメッセージ。
-            off_msg: "自分のページビューを追跡しないに変更する。"  // 3行目に表示させるメッセージ。
+            on_msg: "自分のページビューを追跡に変更する\uff61",  // 2行目に表示させるメッセージ。
+            off_msg: "自分のページビューを追跡しないに変更する\uff61"  // 3行目に表示させるメッセージ。
         };     
         function createElem(tag){  // tagの要素を作成して返す。
             return document.createElement(tag); 
         }
         function trackOff(elems){  // ページビューを追跡していないときの表示を作成。
-            elems[1].textContent = "この"+vars.domein+"はページビューを追跡していません。"; 
+            elems[1].textContent = "この"+vars.domein+"はページビューを追跡していません\uff61"; 
             elems[2].firstChild.onclick = function(){setCookie(2000, elems);return false;};  // aタグのプロパティを設定。
             elems[2].firstChild.textContent = vars.on_msg;
             elems[3].textContent = vars.off_msg;  // aタグがない方のプロパティを設定。
             return [].concat(elems);  // 配列は参照渡しなのでコピーを返す。          
         }
         function trackOn(elems){  // ページビューを追跡しているときの表示を作成。
-            elems[1].textContent = "この"+vars.domein+"はページビューを追跡しています。"; 
+            elems[1].textContent = "この"+vars.domein+"はページビューを追跡しています\uff61"; 
             elems[2].firstChild.onclick = function(){setCookie(2030, elems);return false;};  // aタグのプロパティを設定。
             elems[2].firstChild.textContent = vars.off_msg;
             elems[3].textContent = vars.on_msg;  // aタグがない方のプロパティを設定。
@@ -56,11 +56,12 @@
         } 
         function chTLD(){  // comとjpを入れ替えるaタグが入ったdiv要素を返す。
             var domein = vars.domein;  // ドメイン。
-            domein = (domein.match(/.com$/))?domein.replace(".com",".jp"):domein.replace(".jp",".com");  // ドメインのcomとjpを入れ替える。
+            var flg = /.com$/i.test(domein);  // TLDがcomかの判定。
+            domein = (flg)?domein.replace(".com",".jp"):domein.replace(".jp",".com");  // ドメインのcomとjpを入れ替える。
             var url = document.URL;  // このページのURL。
             var aNode = createElem("a");  // aタグの要素を作成。
-            aNode.href = (domein.match(/.com$/))?url.replace(".com",".jp"):url.replace(".jp",".com/ncr");  // このページのURLのcomとjpを入れ替えてhrefに設定。
-            aNode.textContent = domein + "も設定する。";
+            aNode.href = (flg)?url.replace(".com",".jp"):url.replace(".jp",".com/ncr");  // このページのURLのcomとjpを入れ替えてhrefに設定。
+            aNode.textContent = domein + "も設定する\uff61";
             var divNodeLink = createElem("div");  // aNodeの親要素となるdivタグの要素を作成。
             divNodeLink.appendChild(aNode);  // aNodeをdivNodeLinkの要素の子要素にする。
             divNodeLink.style.paddingTop = "20px";
